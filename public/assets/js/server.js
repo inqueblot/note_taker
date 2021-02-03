ff202aec01b2c56bbdd93da224b84fa97f08d07b
inqueblot <notminebydesign@gmail.com>
2021-02-02 22:05:39 -0600

    express app handles data parsing

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -1,7 +1,20 @@
 var express = require("express");
+var path = require("path")
 
 var app = express();
 var PORT = 3010;
+//space magic
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//routes
+app.get('/notes', function (req, res) {
+    res.
+})
+
+
+
+
 //listener
 app.listen(PORT, function () {
     console.log("App listening on PORT:" + PORT);
