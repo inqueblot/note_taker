06db1d162362c57e920a94244ecf376fc62757af
inqueblot <notminebydesign@gmail.com>
2021-02-02 22:18:16 -0600

    set inital server routes to site htmls

--- public/server.js
+++ public/server.js
@@ -2,16 +2,18 @@ var express = require("express");
 var path = require("path")
 
 var app = express();
-var PORT = 3010;
+var PORT = 3000;
 //space magic
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 //routes
 app.get('/notes', function (req, res) {
-    res.
-})
-
+    res.sendFile(path.join(__dirname, "notes.html"))
+});
+app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, "index.html"))
+});
 
 
 
